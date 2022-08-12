30a3bfef53b8be2e429e7a61f6ad510a58d75326
Boris Shapovalov <bashap@bk.ru>
2022-08-12 15:02:21 +0400

    add view

---
+++ view.py
@@ -0,0 +1,20 @@
+from fractions import Fraction
+import cmath
+import sys
+
+def get_value():
+    value = input('Введите число: ')
+    try:
+        value = Fraction(value)
+        return value
+    except ValueError:
+        value = complex(value)
+        return value
+
+def get_operation():
+    oper = input('Введите оператор: ')
+    return oper
+
+def get_result(res):
+    print(f'Результат: {res}')
+
