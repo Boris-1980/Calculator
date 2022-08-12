d1b0294e272c68f3b5bfd295a47267caf66df54f
Boris Shapovalov <bashap@bk.ru>
2022-08-12 15:01:53 +0400

    add menu

---
+++ menu.py
@@ -0,0 +1,7 @@
+import arithmetic.sum_mod as sum
+import arithmetic.sub_mod as sub
+import arithmetic.mult_mod as mult
+import arithmetic.div_mod as div
+
+dict_arith = {'+': sum, '-': sub, '*': mult, '/': div}
+dict_log = {'+':'сложение','-':'вычитание','*':'умножение','/':'деление'}
